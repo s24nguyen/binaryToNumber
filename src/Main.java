70e559291126b641a730f9d2e1535af9b4f9e386
Steven Nguyen <snguyen@oakland.edu>
2016-01-25 18:23:58 -0500

    Added new method to calculate binaryToNumber

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,17 @@ public class Main {
         String buffer = scanner.nextLine();
         System.out.println("You entered: " + buffer + " for conversion.\n");
         System.out.println("Processing the data...");
+        System.out.println("Decimal number is: " + Binary2Decimal(buffer));
         
+    }
+    private static int Binary2Decimal (String binary){
+        int result = 0;
+        for (int i = 0; i < binary.length(); i++)
+        {
+            int digit = Character.getNumericValue(binary.charAt(i));
+            int position = binary.length()-1-i;
+            result += digit * Math.pow(2,position);
+        }
+        return result;
     }
 }
