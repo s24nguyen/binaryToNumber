b4a8fdf6aa27c17d611aaf964dabc44da78a192b
Steven Nguyen <snguyen@oakland.edu>
2016-01-25 18:01:09 -0500

    Added code to read from the command line

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Binary to Decimal Calculator Program\n");
+        System.out.println("Enter a binary number value: ");
+        Scanner scanner = new Scanner(System.in);
+        String buffer = scanner.nextLine();
+        System.out.println("You entered: " + buffer + " for conversion.\n");
+        System.out.println("Processing the data...");
+        
     }
 }
